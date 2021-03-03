7eedae0feaa366b5afea273fcb435d52bd82d0ad
Saravanan S <selvaraj.saravanan@in.bosch.com>
2021-03-03 18:10:46 +0530

    Renaming done for Main.java

--- vitals/model/BatteryManagementService.java
+++ vitals/model/BatteryManagementService.java
@@ -1,23 +1,13 @@
 package vitals.model;
 
-import java.util.function.Function;
-
 public class BatteryManagementService {
 	public float temperature;
 	public float soc;
 	public float chargeRate;
-	public boolean batteryStatus;
 
 	public BatteryManagementService(float temperature, float soc, float chargeRate) {
 		this.temperature = temperature;
 		this.soc = soc;
 		this.chargeRate = chargeRate;
-		this.batteryStatus = true;
-	}
-
-	public BatteryManagementService validate(BatteryManagementService bms,
-			Function<BatteryManagementService, BatteryManagementService> checkStatus) {
-		checkStatus.apply(this);
-		return bms;
 	}
 }
--- vitals/validator/BMSValidator.java
+++ vitals/validator/BMSValidator.java
@@ -1,42 +1,50 @@
 package vitals.validator;
 
+import java.util.function.Function;
+
 import vitals.constant.BMSFactor;
 import vitals.model.BatteryManagementService;
 
 public class BMSValidator {
-	public static BatteryManagementService isValidTemperature(BatteryManagementService bms) {
-		bms.batteryStatus = bms.batteryStatus && checkRange(BMSFactor.MIN_TEMPERATURE, BMSFactor.MAX_TEMPERATURE,
-				bms.temperature, BMSFactor.TEMPERATURE);
-		return bms;
+
+	public static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
+		BatteryManagementService bms = new BatteryManagementService(temperature, soc, chargeRate);
+		if (isValidTemperature(bms))
+			return true;
+		return false;
 	}
 
-	public static BatteryManagementService isValidStateOfCharge(BatteryManagementService bms) {
-		bms.batteryStatus = bms.batteryStatus
-				&& checkRange(BMSFactor.MIN_SOC, BMSFactor.MAX_SOC, bms.soc, BMSFactor.SOC);
-		return bms;
-	}
+	public static Function<BatteryManagementService, Boolean> isValidChargeRate = (BatteryManagementService bms) -> {
+		return checkChargeRate(BMSFactor.MAX_CHANGE_RATE, bms.chargeRate, BMSFactor.CHARGE_RATE);
+	};
 
-	public static BatteryManagementService isValidChargeRate(BatteryManagementService bms) {
-		bms.batteryStatus = bms.batteryStatus
-				&& checkChargeRate(BMSFactor.MAX_CHANGE_RATE, bms.chargeRate, BMSFactor.CHARGE_RATE);
-		return bms;
-	}
+	public static Function<BatteryManagementService, Boolean> isValidStateOfCharge = (BatteryManagementService bms) -> {
+		if (checkRange(BMSFactor.MIN_SOC, BMSFactor.MAX_SOC, bms.soc, BMSFactor.SOC)) {
+			return isValidChargeRate.apply(bms);
+		}
+		return false;
+	};
 
-	public static boolean checkRange(float minVal, float maxVal, float value, String factorName) {
+	public static Boolean isValidTemperature(BatteryManagementService bms) {
+		if (checkRange(BMSFactor.MIN_TEMPERATURE, BMSFactor.MAX_TEMPERATURE, bms.temperature, BMSFactor.TEMPERATURE)) {
+			return isValidStateOfCharge.apply(bms);
+		}
+		return false;
+	};
+
+	public static Boolean checkRange(float minVal, float maxVal, float value, String factorName) {
 		if (value < minVal || value > maxVal) {
 			printStatus(factorName, (value > maxVal));
 			return false;
 		}
-
 		return true;
 	}
 
-	static boolean checkChargeRate(float maxVal, float value, String factorName) {
+	static Boolean checkChargeRate(float maxVal, float value, String factorName) {
 		if (value > maxVal) {
 			printStatus(factorName, (value > maxVal));
 			return false;
 		}
-
 		return true;
 	}
 
